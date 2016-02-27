a038d56dac2925654fd31ffecdb9d3d2de20b8f4
mkrooted <koreshov.m@gmail.com>
2016-02-27 18:12:24 +0200

    API for 'users' completed

--- v1/app.js
+++ v1/app.js
@@ -1,97 +1,226 @@
-// Created by mkrooted on 21.02.2016.
+// Lincoln_API Project. github.com/mkrooted/lincoln_api
 
 var util = require("util");
 var express = require("express");
 var validator = require("validator");
 var crypto = require("crypto");
+var bodyParser = require('body-parser');
+var regexp = require("node-regexp");
+
 var app = express();
 app.locals.title = "Lincoln_API";
 app.locals.email = "koreshov.m@gmail.com";
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
 
-var pool = require("mysql").createPool({
-    host: "localhost",
-    port: "3306",
-    user: "dev",
-    password: "mlg_quickscoper",
-    database: "lincoln_project",
-    connectionLimit: 20,
-    debug: false
-});
+function getConnection(callback) {
+    var conn;
+    try {
+        conn = require("mysql").createConnection({ // pool for mysql connections
+            host: "localhost",
+            port: "3306",
+            user: "dev",
+            password: "mlg_quickscoper",
+            database: "lincoln_project",
+            multipleStatements: true,
+            debug: false
+        });
+    } catch (err) {
+        callback(err);
+    }
+    return conn;
+}
 
 var connectionCount = 0;
 
-function getNewHash() {
+function getNewHash() //generates random 20 bytes and returns their HEX representation
+{
     return crypto.randomBytes(20).toString('hex');
 }
 
+// USERS SECTION
 app.get("/users", function (req, res) {
     res.type('text/plain');
-    pool.getConnection(function (error) {
+    var conn = getConnection(function (error) {
         if (error) {
             console.error("Error connecing to database: " + error.stack);
             res.statusCode = 500;
             res.send("{error: 500, desc: 'Error connecting to DB: " + error.stack + "'}");
             return;
         }
-        console.log("Connection to dev@localhost successful! Connection ID: " + pool.threadId);
-    });
-
-    pool.query("SELECT * FROM users", function (err, rows, fields) {
+        console.log("Connection to dev@localhost successful! Connection ID: " + conn.threadId);
+    });//connect to the database and throw ACHTUNG 500 INTERNAL SERVER ERROR if error
+    conn.query("SELECT * FROM users", function (err, rows, fields) {
         res.statusCode = 200;
-        var r = "[\n";
+        res.type("application/json");
+        var r = "[";
         rows.forEach(function (i) {
-            r += "{id: '" + i.user_id + "', login: '" + i.user_login + "', password: '" +
-                i.user_password + "', hash: '" + i.user_hash + "', last_ip: '" + i.user_last_ip +
-                "', email: '" + i.user_email + "', date_registered: '" + i.user_date_registered + "', last_login: '" +
-                i.user_date_last_login + "', wmid: '" + i.user_wmid + "}\n";
+            r += "{\"login\": \"" + i.user_login + "\", \"password\": \"" +
+                i.user_password + "\", \"hash\": \"" + i.user_hash + "\", \"last_ip\": \"" + i.user_last_ip +
+                "\", \"email\": \"" + i.user_email + "\", \"date_registered\": \"" + i.user_date_registered + "\", \"last_login\": \"" +
+                i.user_date_last_login + "\", \"wmid\": \"" + i.user_wmid + "\"},";
 
         });
-        r += "]";
-        res.send(r);
-    });
-});
-app.get("/users/:id", function (req, res) {
+        res.send(r.substring(0, r.length - 1) + "]");
+        conn.end();
+    });//if conected successfully get all records from table
+}); //get all records from table 'users'
+app.get("/users/:login", function (req, res) {
     res.type('text/plain');
-    pool.getConnection(function (error) {
+    var conn = getConnection(function (error) {
+        if (error) {
+            console.error("Error connecing to database: " + error.stack);
+            res.statusCode = 500;
+            res.json({'error': 500, 'desc': 'Error connecting to DB: ' + error.stack});
+            conn.end();
+            return;
+        }
+        console.log("Connection to dev@localhost successful! Connection ID: " + conn.threadId);
+    });//connect to the database and throw ACHTUNG 500 INTERNAL SERVER ERROR if error
+    conn.query("SELECT * FROM users WHERE user_login='" + req.params.login + "'", function (err, rows, fields) {
+        if (util.isUndefined(rows[0])) {
+                res.statusCode = 404;
+            res.send("{error: 404, desc:'User with login " + req.params.login + " not found'}");
+            conn.end();
+                return;
+            }
+            var i = rows[0];
+        var r = "{\"login\": \"" + i.user_login + "\", \"password\": \"" +
+            i.user_password + "\", \"hash\": \"" + i.user_hash + "\", \"last_ip\": \"" + i.user_last_ip +
+            "\", \"email\": \"" + i.user_email + "\", \"date_registered\": \"" + i.user_date_registered + "\", \"last_login\": \"" +
+            i.user_date_last_login + "\", \"wmid\": \"" + i.user_wmid + "\"}";
+        res.type("application/json");
+            res.send(r);
+        conn.end();
+        });
+}); //get certain user by login
+app.post("/users", function (req, res) {
+    var data = req.body;
+    var conn = getConnection(function (error) {
         if (error) {
             console.error("Error connecing to database: " + error.stack);
             res.statusCode = 500;
             res.send("{error: 500, desc: 'Error connecting to DB: " + error.stack + "'}");
             return;
         }
-        console.log("Connection to dev@localhost successful! Connection ID: " + pool.threadId);
-    });
-    if (validator.isNumeric(req.params.id)) {
-        pool.query("SELECT * FROM users WHERE user_id=" + req.params.id, function (err, rows, fields) {
-            if (util.isNullOrUndefined(rows[0])) {
-                res.statusCode = 404;
-                res.send("{error: 404, desc:'User with id " + req.params.id + " not found'}");
-                return;
-            }
-            var i = rows[0];
-            var r = "{id: '" + i.user_id + "', login: '" + i.user_login + "', password: '" +
-                i.user_password + "', hash: '" + i.user_hash + "', last_ip: '" + i.user_last_ip +
-                "', email: '" + i.user_email + "', date_registered: '" + i.user_date_registered + "', last_login: '" +
-                i.user_date_last_login + "', wmid: '" + i.user_wmid + "}";
-            res.send(r);
-        });
+        console.log("Connection to dev@localhost successful! Connection ID: " + conn.threadId);
+    });//connect to the database and throw ACHTUNG 500 INTERNAL SERVER ERROR if error
+    if (util.isNullOrUndefined(data.login) || util.isNullOrUndefined(data.email) || util.isNullOrUndefined(data.password)
+        || util.isNullOrUndefined(data.wmid)) {
+        res.statusCode = 400;
+        res.send("{error: 400, desc:'Incomplete request. Cannot create user'}");
+        conn.end();
+        return;
     } else {
-        pool.query("SELECT * FROM users WHERE user_login='" + req.params.id + "'", function (err, rows, fields) {
-            if (util.isUndefined(rows[0])) {
-                res.statusCode = 404;
-                res.send("{error: 404, desc:'User with login " + req.params.id + " not found'}");
+        conn.query("INSERT INTO users(`user_login`,`user_email`,`user_password`,`user_wmid`,`user_last_ip`) " +
+            "VALUES('" + data.login + "','" + data.email + "','" + data.password + "','" + data.wmid + "','" + req.ip + "');", function (err) {
+            if (err) {
+                if (err.stack.indexOf("ER_DUP_ENTRY") > -1) {
+                    res.statusCode = 409;
+                    res.json({"error": 409, "desc": "Login taken"});
+                } else {
+                    console.error("Error creating user from ip " + req.ip + "; Desc: " + err.stack);
+                    res.statusCode = 500;
+                    res.json({
+                        "error": 500,
+                        "desc": "Something gone wrong while creating user record",
+                        "stack": err.stack
+                    });
+                }
+                conn.end();
                 return;
-            }
-            var i = rows[0];
-            var r = "{id: '" + i.user_id + "', login: '" + i.user_login + "', password: '" +
-                i.user_password + "', hash: '" + i.user_hash + "', last_ip: '" + i.user_last_ip +
-                "', email: '" + i.user_email + "', date_registered: '" + i.user_date_registered + "', last_login: '" +
-                i.user_date_last_login + "', wmid: '" + i.user_wmid + "}";
-            res.send(r);
+            } else res.sendStatus(200);
+            conn.end();
         });
     }
-
 });
+app.patch("/users/:login", function (req, res) {
+    var conn = getConnection(function (error) {
+        if (error) {
+            console.error("Error connecing to database: " + error.stack);
+            res.statusCode = 500;
+            res.json({'error': 500, 'desc': 'Error connecting to DB: ' + error.stack});
+            conn.end();
+            return;
+        }
+        console.log("Connection to dev@localhost successful! Connection ID: " + conn.threadId);
+    });//connect to the database and throw ACHTUNG 500 INTERNAL SERVER ERROR if error
+
+    conn.query("SELECT * FROM users WHERE user_login='" + req.params.login + "'", function (err, rows, fields) {
+        if (util.isUndefined(rows[0])) {
+            res.statusCode = 404;
+            res.send("{error: 404, desc:'User with login " + req.params.login + " not found'}");
+            conn.end();
+            return;
+        }
+    });
+
+    var data = req.body;
+    var sql = "";
+
+    if (!util.isUndefined(data.email)) {
+        sql += "UPDATE `users` SET `user_email`='" + data.email + "' WHERE `user_login`='" + req.params.login + "';\n";
+    }
+    if (!util.isUndefined(data.password)) {
+        sql += "UPDATE `users` SET `user_password`='" + data.password + "' WHERE `user_login`='" + req.params.login + "';\n";
+    }
+    if (!util.isUndefined(data.hash)) {
+        sql += "UPDATE `users` SET `user_hash`='" + data.hash + "' WHERE `user_login`='" + req.params.login + "';\n";
+    }
+    if (!util.isUndefined(data.last_ip)) {
+        sql += "UPDATE `users` SET `last_ip`='" + data.last_ip + "' WHERE `user_login`='" + req.params.login + "';\n";
+    }
+    if (!util.isUndefined(data.deleted)) {
+        sql += "UPDATE `users` SET `user_deleted`=" + data.deleted + " WHERE `user_login`='" + req.params.login + "';\n";
+    }
+    if (!util.isUndefined(data.last_login)) {
+        sql += "UPDATE `users` SET `user_date_last_login`=NOW() WHERE `user_login`='" + req.params.login + "';\n";
+    }
+    if (!util.isUndefined(data.wmid)) {
+        sql += "UPDATE `users` SET `user_wmid`='" + data.wmid + "' WHERE `user_login`='" + req.params.login + "';\n";
+    }
+    conn.query(sql, function (err, rows, fields) {
+        if (err) {
+            res.statusCode = 500;
+            res.json({"error": 500, "desc": "Something gone wrong while executing query", "stack": err.stack});
+            conn.end();
+            return;
+        }
+    });
+
+    res.sendStatus(200);
+    conn.end();
+});
+app.delete("/users/:login", function (req, res) {
+    var conn = getConnection(function (error) {
+        if (error) {
+            console.error("Error connecing to database: " + error.stack);
+            res.statusCode = 500;
+            res.json({'error': 500, 'desc': 'Error connecting to DB: ' + error.stack});
+            conn.end();
+            return;
+        }
+        console.log("Connection to dev@localhost successful! Connection ID: " + conn.threadId);
+    });//connect to the database and throw ACHTUNG 500 INTERNAL SERVER ERROR if error
+    conn.query("SELECT * FROM users WHERE user_login='" + req.params.login + "'", function (err, rows, fields) {
+        if (util.isUndefined(rows[0])) {
+            res.statusCode = 404;
+            res.send("{error: 404, desc:'User with login " + req.params.login + " not found'}");
+            conn.end();
+            return;
+        }
+    });
+
+    conn.query("DELETE FROM `users` WHERE user_login='" + req.params.login + "'", function (err, rows, fields) {
+        if (err) {
+            res.statusCode = 500;
+            res.json({"error": 500, "desc": "Something gone wrong while executing query", "stack": err.stack});
+        }
+    })
+    conn.end();
+    res.sendStatus(200);
+});
+
 app.get("/sales", function (req, res) {
     res.type('text/plain');
     pool.getConnection(function (error) {
@@ -100,7 +229,7 @@ app.get("/sales", function (req, res) {
             res.statusCode = 500;
             return
         }
-        console.log("Connection to dev@localhost successful! Connection ID: " + pool.threadId);
+        console.log("Connection to dev@localhost successful! Connection ID: " + conn.threadId);
     });
 
     pool.query("SELECT * FROM sales", function (err, rows, fields) {
